68af65a8994c1013a25779710543b39175e8eae5
Vitor Soares Pereira <vitor.pereira@smart01.com.br>
2025-02-04 21:03:38 -0300

    Initial project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header";
+// import { Post } from './Post';
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {/* <Post 
+            author="Diego Fernandades"
+            content="ipsum dolor sit amet consectetur adipisicing elit. Nobis porro eum vero quo, officia commodi quasi similique at? Animi velit consectetur iusto atque! Tempora esse temporibus sunt dignissimos commodi rem."
+          />
+          <Post 
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          /> */}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
